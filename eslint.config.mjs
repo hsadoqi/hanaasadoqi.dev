250281f6c1bd1575a7a992f12fe1c099c31606e4
Hanaa Sadoqi <hsadoqi@gmail.com>
2026-06-24 14:47:45 -0400

    feat: update configuration files to include docs and add next-themes dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,15 @@ import nextTs from 'eslint-config-next/typescript';
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'dist/**',
+    'node_modules/**',
+    'coverage/**',
+    'public/**',
+    'docs/**',
+  ]),
 ]);
